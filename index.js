e5504f9f544a812a09bf97fe55f15aff7b7e86a7
dMuguti <omiahderek56@gmail.com>
2023-04-02 00:23:10 +0300

    commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors())
 
 let rawdata = fs.readFileSync('index.json');
     let movies = JSON.parse(rawdata);
